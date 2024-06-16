82a23fff3b9dc11cae93a483cd8407e75940bafa
Saumya Raj <146992770+saumyaraj0904@users.noreply.github.com>
2024-06-16 19:16:12 +0530

    Intial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { TicTacToe } from './components/TicTacToe/TicTacToe';
+
+function App() {
+  return (
+    <div className="App">
+    <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
